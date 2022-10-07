4a58b98428d00005fcffe372fdb29cde9b6bb180
Daniele Ciciani <dciciani@cloudera.com>
2022-10-07 22:10:13 +0000

    chekpoint

---
+++ model/.ipynb_checkpoints/model_endpoint-checkpoint.py
@@ -0,0 +1,44 @@
+import mlflow
+import pandas as pd
+import cdsw, numpy, sklearn
+from cmlapi.utils import Cursor
+
+# path miglior modello MLFlow preso come argomento
+#logged_model = sys.argv[1]
+
+logged_model = '/home/cdsw/.experiments/n03k-3b0z-nbdp-wz8l/94k7-jivb-s3j1-g7r9/artifacts/model'
+
+"""
+data = {
+    
+  'sensor_00' : '4219',
+  'sensor_02' : '31294',
+  'sensor_04' : '421',
+  'sensor_06' : '645',
+  'sensor_07' : '664',
+  'sensor_08' : '7654',
+  'sensor_09' : '12',
+  'sensor_10' : '1321',
+  'sensor_11' : '3124',
+  'sensor_51' : '643',
+}
+"""
+
+@cdsw.model_metrics
+def predict(data):
+    
+    df = pd.DataFrame(data, index=[0])
+    df.columns = ['sensor_00', 'sensor_02', 'sensor_04', 'sensor_06', 'sensor_07',
+                  'sensor_08', 'sensor_09', 'sensor_10', 'sensor_11', 'sensor_51']
+    
+    #data = args.get('input')
+    # Load model as a PyFuncModel.
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+ 
+    # Predict on a Pandas DataFrame.
+    pred = loaded_model.predict(df)
+    
+    cdsw.track_metric("prediction", str(pred))
+    cdsw.track_metric("data", data)
+   
+    return {'input_data': str(data), 'pred': str(pred[0])}
--- model/model_endpoint.py
+++ model/model_endpoint.py
@@ -3,20 +3,24 @@ import pandas as pd
 import cdsw, numpy, sklearn
 from cmlapi.utils import Cursor
 
+# path miglior modello MLFlow preso come argomento
+#logged_model = sys.argv[1]
+
 logged_model = '/home/cdsw/.experiments/n03k-3b0z-nbdp-wz8l/94k7-jivb-s3j1-g7r9/artifacts/model'
 
 """
 data = {
     
-  'sensor_04' : '4219',
-  'sensor_19' : '31294',
-  'sensor_20' : '421',
-  'sensor_21' : '645',
-  'sensor_38' : '664',
-  'sensor_39' : '7654',
-  'sensor_40' : '12',
-  'sensor_41' : '1321',
-  'sensor_42' : '3124',
+  'sensor_00' : '4219',
+  'sensor_02' : '31294',
+  'sensor_04' : '421',
+  'sensor_06' : '645',
+  'sensor_07' : '664',
+  'sensor_08' : '7654',
+  'sensor_09' : '12',
+  'sensor_10' : '1321',
+  'sensor_11' : '3124',
+  'sensor_51' : '643',
 }
 """
 
@@ -24,8 +28,9 @@ data = {
 def predict(data):
     
     df = pd.DataFrame(data, index=[0])
-    df.columns = ['sensor_04', 'sensor_19', 'sensor_20', 'sensor_21', 'sensor_38', 'sensor_39', 'sensor_40', 'sensor_41', 'sensor_42']
-
+    df.columns = ['sensor_00', 'sensor_02', 'sensor_04', 'sensor_06', 'sensor_07',
+                  'sensor_08', 'sensor_09', 'sensor_10', 'sensor_11', 'sensor_51']
+    
     #data = args.get('input')
     # Load model as a PyFuncModel.
     loaded_model = mlflow.pyfunc.load_model(logged_model)
