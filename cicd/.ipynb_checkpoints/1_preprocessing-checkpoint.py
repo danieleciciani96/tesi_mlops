d48543ab3ab082e5e26028337da0f007501f4133
Daniele Ciciani <dciciani@cloudera.com>
2022-10-07 23:55:07 +0000

    iceberg time travel

--- cicd/.ipynb_checkpoints/1_preprocessing-checkpoint.py
+++ cicd/.ipynb_checkpoints/1_preprocessing-checkpoint.py
@@ -24,7 +24,7 @@ spark = SparkSession.builder\
 ## Load data from Iceberg
 df_raw = spark.sql("SELECT * FROM spark_catalog.default.pump_raw")
 
-# Adjust target class, NORMAL = 1 and RECOVERING & BROKEN = 0
+# NORMAL = 1 and RECOVERING & BROKEN = 0
 df = df_raw.withColumn('machine_status_tmp', when(df_raw.machine_status == "NORMAL", 1)\
                                     .when(df_raw.machine_status == "RECOVERING", 0)\
                                     .when(df_raw.machine_status == "BROKEN", 0)\
@@ -40,7 +40,7 @@ print(df.groupBy('machine_status').count().orderBy('count').show())
 # Fill na values with -1 
 df = df.na.fill(value=-1)
 
-# Select only the relevant Feature
+# Select the relevant Feature
 final_sensors = ['sensor_00','sensor_02', 'sensor_04', 'sensor_06',  'sensor_07', 'sensor_08',
                  'sensor_09', 'sensor_10', 'sensor_11', 'sensor_51', 'machine_status']
 
@@ -55,10 +55,9 @@ try:
 except:
     spark.sql("drop table spark_catalog.default.pump_processed")
     df.writeTo("spark_catalog.default.pump_processed").using("iceberg").create()
-    df.toPandas().to_csv('/home/cdsw/data/pump_sensors_processed.csv', index=False)    
+    df.toPandas().to_csv('/home/cdsw/data/pump_sensors_processed.csv', index=False)
     
 spark.stop()
-
 
 
 """
---
+++ cicd/.ipynb_checkpoints/2_testing-checkpoint.py
@@ -0,0 +1,31 @@
+#The shape of your data
+import unittest
+import pytest
+import pandas as pd
+
+path_processed = "/home/cdsw/data/pump_processed.csv"
+        
+data = pd.read_csv(path_processed)
+data.drop("Unnamed: 0", axis=1, inplace=True)
+
+def test_n_columns(data):
+    n_cols = data.shape[1]
+    assert n_cols == 10
+    
+    
+def test_min_max_s4(s4):
+    min_allowed = 0
+    max_allowed = 1000000
+    
+    assert s4.describe().max() <= max_allowed
+    assert s4.describe().min() > min_allowed
+    
+def test_target(target):
+    assert target.unique()[0] == 1
+    assert target.unique()[1] == 0
+    
+    
+
+test_n_columns(data)
+test_min_max_s4(data.sensor_04)
+test_target(data.machine_status)
