d87f1bea4b9da9367ad8b6428ab60b76a0670274
Daniele Ciciani <dciciani@cloudera.com>
2022-10-14 14:00:20 +0000

    last updates before send

--- cicd/2_testing.py
+++ cicd/2_testing.py
@@ -37,7 +37,35 @@ def test_target(target):
     assert target.unique()[1] == 0
     
     
-
 test_n_columns(data)
 test_min_max_s4(data.sensor_04)
 test_target(data.machine_status)
+
+"""
+#to do: pylint quality check
+
+from pylint.lint import Run
+import sys
+import json
+
+def quality_check(pyfile):
+
+    THRESHOLD = 5
+    if len(sys.argv) < 2:
+        print("Module to evaluate need to be the first argument")
+
+    run = Run(['--errors-only', '--output-format=json', pyfile], do_exit=False)
+    score = run.linter.stats['global_note']"
+
+    outputjson = run.linter.stats['by_module']
+    outputjson['score'] = score
+
+    with open('log_error.json', 'w') as outfile:
+        json.dump(outputjson, outfile)
+
+    if score < THRESHOLD:
+        sys.exit
+
+quality_check(sys.argv[1])
+
+"""
